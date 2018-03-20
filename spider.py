ad456343595db2c4709e45d808780814b32a9a07
KeyListener <872490934@qq.com>
2018-03-20 22:45:32 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -9,12 +9,18 @@ from bs4 import BeautifulSoup
 
 def del_tag(strings):
 	dr = re.compile(r'<[^>]+>',re.S)
-	strs = []
-	for string in strings:
-		string = str(string)
-		s = dr.sub('',string)
-		strs.append(s)
-	return strs
+	if type(strings) == type([]): 
+		strs = []
+		for string in strings:
+			string = str(string)
+			s = dr.sub('',string)
+			strs.append(s)
+		return strs
+	else:
+		strings= str(strings)
+		s = dr.sub('',strings)
+		return s
+
 
 path = '/home/wangfeihong/桌面/support.huaweicloud.com/'
 
@@ -24,7 +30,7 @@ for file in tqdm(files):
 	data = {}	
 	f = open(path + file,mode = 'r')
 	text = f.read()
-	# print(file) 	
+	print(file) 	
 	if not 'developer' in file:
 		soup = BeautifulSoup(text,'lxml')
 		elements = soup.select('.help-link')
@@ -42,7 +48,6 @@ for file in tqdm(files):
 		else:
 			data['title'] = 'null'
 		data['desc'] = t
-
 		details = soup.select('.help-details')
 		if len(details) == 0:
 			details = soup.select('div[id^="body"]') 
@@ -59,75 +64,84 @@ for file in tqdm(files):
 		if len(details) == 0:
 			continue
 		# 正常是所有页面都有一个div储存问答信息,类名不一定是什么，大概就上面几种
+		soup = BeautifulSoup(str(details),'lxml')
+		if len(descs) == 0:
+			h1 = soup.h1
+			siblings = []
+			if len(h1) > 0:
+				for s in h1.next_siblings:
+					siblings.append(s)
+			h4s = soup.select('h4')
+			h3s = soup.select('h3')
+			hs = h3s + h4s
+			qas = []
+			if len(hs) > 0:
+				for h in hs:
+					for s in h.next_siblings:
+						if not s.isspace:
+							qas.append({'question':del_tag(h),'answer':del_tag(s)})
+			dls = soup.select('dl')
+			if len(dls) > 0:
+				for dl in dls:
+					for s in dl.next_siblings:
+						if not s.isspace:
+							soup2 = BeautifulSoup(str(dl),'lxml')
+							dts = soup2.dt
+							dds = soup2.select('dd')
+							dds = del_tag(dds)
+							qas.append({'question':del_tag(dts),'answer':del_tag(s)}) 
+			data['qas'] = qas						
+			print(data)
 
-		qas = []
-		soup2 = BeautifulSoup(str(details),'lxml')
-		sections = soup2.select('.section')	
-		# 分两类,有无section的情况
-		# print(len(sections))
-		data['intro'] = []	
-		string1 = str(details).split('<div')
-		if len(string1) > 2:
-			data['intro'] = (del_tag(['<'+string1[2]]))
-		else:
-			data['intro'] = (del_tag(['<'+string1[len(string1)-1]]))
-		# 这是总标题的解释，位于总标题的下端
-		if len(sections) > 0:
-			# 每个<h4>和<p><table>等一些列作为答案的标签一一对应
-			for section in sections:
-				soup3 = BeautifulSoup(str(section),'lxml')
-				h4s = soup3.select('h4')
-				if len(h4s) != 0 :
-					for h4 in h4s:
-						answer = str(section).split(str(h4))[1]
-						answer = del_tag([answer])
-						h4 = del_tag([h4])
-						qa = {'question':h4[0],'answer':answer}
-						qas.append(qa)
-		else:
-			uls = soup2.select('ul')	
-			ps = soup2.select('p')
-			tables = soup2.select('table')
-			pres = soup2.select('pre')
-			uls = del_tag(uls)
-			ps = del_tag(ps)
-			pres = del_tag(pres)
-			tables = del_tag(tables)
-			if len(ps) > 0:
-				del ps[0]
-			qa = {'question':data['title'],'answer':{'table':tables,'ul':uls,'p':ps,'pre':pres}}
-		
-		qas.append(qa)	
-		data['qas'] = qas
-		# print(data)
-	
+			break
+
+
+
+	# developer
 	else:
 		soup = BeautifulSoup(text,'lxml')
 		details = soup.select('#content')
 		soup = BeautifulSoup(str(details),'lxml')
-		block_titles = soup.select('block-title')
 		descs = soup.select('.crumbs')
 		titles = soup.select('span')
 		# 找到所有h3作为问题，再找他的兄弟节点作为答案
 		if len(descs) == 0:
-			captions = soup.select('.caption-main')
-			for caption in captions:
-				for s in caption.next_siblings:
-					print(s) 
-					
+			h1 = soup.h1
+			siblings = []
+			if len(h1) > 0:
+				for s in h1.next_siblings:
+					siblings.append(s)
+			h4s = soup.select('h4')
+			h3s = soup.select('h3')
+			h1s = soup.select('h1')
+			hs = h1s + h3s + h4s
+			qas = []
+			if len(hs) > 0:
+				for h in hs:
+					for s in h.next_siblings:
+						if not s.isspace:
+							qas.append({'question':del_tag(h),'answer':del_tag(s)})
+			dls = soup.select('dl')
+			if len(dls) > 0:
+				for dl in dls:
+					for s in dl.next_siblings:
+						if not s.isspace:
+							soup2 = BeautifulSoup(str(dl),'lxml')
+							dts = soup2.dt
+							dds = soup2.select('dd')
+							dds = del_tag(dds)
+							qas.append({'question':del_tag(dts),'answer':del_tag(s)}) 
+			data['qas'] = qas						
 			data['title'] = del_tag(soup.select('.poster-caption'))[0]
-			# h1s = soup. 
-			pass
+			data['desc'] = data['title']
 		else:
-			print(file)
-			texts = soup.select('.text')
-			desc = descs[0].get_text()
-			texts = del_tag(texts)
-			data['desc'] = desc
-			data['title'] = del_tag(titles)[0]
-			data['qas'] = {'question':desc,'answer':texts[0]}
-			# print(data['title'])
+			pass
 			# print(file)
-
+			# texts = soup.select('.text')
+			# desc = descs[0].get_text()
+			# data['desc'] = desc
+			# data['title'] = del_tag(titles[0])
+			# data['qas'] = {'question':desc,'answer':del_tag(texts[0])}
+			# print(data)
 
 
