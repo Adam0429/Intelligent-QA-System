fe9e57c17f78f8cafa8e37c6b13fc310000d63d0
KeyListener <15205902@ucdconnect.ie>
2018-03-28 20:05:26 +0800

    DESC

--- spider.py
+++ spider.py
@@ -26,12 +26,12 @@ def del_tag(strings):
 path = '/home/wangfeihong/桌面/support.huaweicloud.com/'
 
 files = os.listdir(path)
-
+# txt = open('1.txt','w+')
 for file in tqdm(files):
 	data = {}	
 	f = open(path + file,mode = 'r')
 	text = f.read()
-	print(file) 	
+	print(file)
 	if not 'developer' in file:
 		soup = BeautifulSoup(text,'lxml')
 		elements = soup.select('.help-link')
@@ -44,15 +44,11 @@ for file in tqdm(files):
 		ele = soup.select('.topictitle1')
 		title = del_tag(ele)
 		soup2 = BeautifulSoup(text,'lxml')
-		# descs = soup2.select('.crumbs')
-		# data['desc'] = del_tag(descs)
-
 		if len(title) == 1:
 			data['title'] = title[0]
 			t.append(data['title'])
 		else:
 			data['title'] = 'null'
-		# data['desc'] = t
 		details = soup.select('.help-details')
 		if len(details) == 0:
 			details = soup.select('div[id^="body"]') 
@@ -73,73 +69,74 @@ for file in tqdm(files):
 		soup = BeautifulSoup(str(details),'lxml')
 		soup2 = BeautifulSoup(text,'lxml')
 		descs = soup2.select('.crumbs')
-		#----------------------
-		soup3 = BeautifulSoup(str(descs[0]),'lxml')
-		aa = soup3.select('a')
-		data['desc'] = del_tag(aa)
+		# soup3 = BeautifulSoup(str(descs),'lxml')
+		desc = []
 		if len(descs) == 0:
-			h1 = soup.h1
-			siblings = []
-			if h1 is None:
-			# some index pages
-				soup2 = BeautifulSoup(text,'lxml')
-				ps = soup.select('p')
-				data['qas'] = {'question':del_tag(soup2.select('.text')[0]),'answer':del_tag(ps[1:])}
-				if len(soup2.select('.beg-title')) == 0:
-					data['title'] = 'null'
-				else:
-					data['title'] = del_tag(soup2.select('.beg-title')[0])
-				print(data)
-			else:
-				if len(h1) > 0:
-					for s in h1.next_siblings:
-						siblings.append(s)
-				h4s = soup.select('h4')
-				h3s = soup.select('h3')
+			descs = soup2.select('.position')
+		if len(descs) != 0:
+			for c in descs[0].children:
+				if not c.isspace:
+					desc.append(del_tag(c))
+			data['desc'] = desc
+		h1 = soup.h1
 
-				hs = h3s + h4s
-				qas = []
-				if len(hs) > 0:
-					for h in hs:
-						for s in h.next_siblings:
-							if not s.isspace:
-								qas.append({'question':del_tag(h),'answer':del_tag(s)})
-				dls = soup.select('dl')
-				if len(dls) > 0:
-					for dl in dls:
-						for s in dl.next_siblings:
-							if not s.isspace:
-								soup2 = BeautifulSoup(str(dl),'lxml')
-								dts = soup2.dt
-								dds = soup2.select('dd')
-								dds = del_tag(dds)
-								qas.append({'question':del_tag(dts),'answer':del_tag(s)}) 
-				data['qas'] = qas			
-				print(data['desc'])
-		else:
-			soup2 = BeautifulSoup(text,'lxml')
-			descs = soup2.select('.crumbs')
+		if h1 is None:
+		# some index pages
+
 			ps = soup.select('p')
+			data['qas'] = {data['desc'][len(data['desc'])-1]:del_tag(ps[1:])}
+			if len(soup2.select('.beg-title')) == 0:
+				data['title'] = 'null'
+			else: 
+				data['title'] = del_tag(soup2.select('.beg-title')[0])
+			
+			h5s = soup.select('h5')
 			h4s = soup.select('h4')
 			h3s = soup.select('h3')
-			hs = h3s + h4s
-			qas = []
+			h1s = soup.select('h1')
+			hs = h1s + h3s + h4s + h5s
+			qas = {}
 			if len(hs) > 0:
 				for h in hs:
 					for s in h.next_siblings:
 						if not s.isspace:
-							qas.append({'question':del_tag(h),'answer':del_tag(s)})
-			dls = soup.select('dl')
-			if len(dls) > 0:
-				for dl in dls:
-					for s in dl.next_siblings:
+							if del_tag(h) not in qas.keys():
+								qas[del_tag(h)] = del_tag(s)
+			# txt.writelines("-".join(str(data['desc'])))
+			# txt.writelines('\n')
+			# data['qas'] = qas			
+			# print(data['qas'])
+		else:
+			ps = soup.select('p')
+			h5s = soup.select('h5')
+			h4s = soup.select('h4')
+			h3s = soup.select('h3')
+			h1s = soup.select('h1')
+			hs = h1s + h3s + h4s + h5s
+			qas = {}
+			if len(hs) > 0:
+				for h in hs:
+					for s in h.next_siblings:
 						if not s.isspace:
-							soup2 = BeautifulSoup(str(dl),'lxml')
-							dts = soup2.dt
-							dds = soup2.select('dd')
-							dds = del_tag(dds)
-							qas.append({'question':del_tag(dts),'	answer':del_tag(s)}) 
-			print(data['desc'])
+							if del_tag(h) not in qas.keys():
+								qas[del_tag(h)] = del_tag(s)
+			# dls = soup.select('dl')
+			# if len(dls) > 0:
+			# 	for dl in dls:
+			# 		for s in dl.next_siblings:
+			# 			if not s.isspace:
+			# 				soup2 = BeautifulSoup(str(dl),'lxml')
+			# 				dts = soup2.dt
+			# 				dds = soup2.select('dd')
+			# 				dds = del_tag(dds)
+			# 				qas.append({'question':del_tag(dts),'answer':del_tag(s)}) 
+			data['qas'] = qas
+			# for key,value in qas.items():
+			# 	print(key)
+			# 	print('========================================')
+			# 	print(value)
+			# txt.writelines("".join(str(data['desc'])))
+			# txt.writelines('\n')
 	# developer
 	else:
 		soup = BeautifulSoup(text,'lxml')
@@ -147,19 +144,22 @@ for file in tqdm(files):
 		soup = BeautifulSoup(str(details),'lxml')
 		descs = soup.select('.crumbs')
 		titles = soup.select('span')
+		h1 = soup.h1
 		# 找到所有h3作为问题，再找他的兄弟节点作为答案
 		if len(descs) == 0:
-			h1 = soup.h1
-			siblings = []
-			if h1 is None:
-				continue
-			if len(h1) > 0:
-				for s in h1.next_siblings:
-					siblings.append(s)
+			descs = soup.select('.position')
+		if len(descs) != 0:
+			for c in descs[0].children:
+				if not c.isspace:
+					desc.append(del_tag(c))
+			data['desc'] = desc
+		if h1 is None:
+			continue
+			h5s = soup.select('h5')
 			h4s = soup.select('h4')
 			h3s = soup.select('h3')
 			h1s = soup.select('h1')
-			hs = h1s + h3s + h4s
+			hs = h1s + h3s + h4s + h5s
 			qas = []
 			if len(hs) > 0:
 				for h in hs:
@@ -167,26 +167,21 @@ for file in tqdm(files):
 						if not s.isspace:
 							qas.append({'question':del_tag(h),'answer':del_tag(s)})
 			dls = soup.select('dl')
-			if len(dls) > 0:
-				for dl in dls:
-					for s in dl.next_siblings:
-						if not s.isspace:
-							soup2 = BeautifulSoup(str(dl),'lxml')
-							dts = soup2.dt
-							dds = soup2.select('dd')
-							dds = del_tag(dds)
-							qas.append({'question':del_tag(dts),'answer':del_tag(s)}) 
-			data['qas'] = qas						
-			data['title'] = del_tag(soup.select('.poster-caption'))[0]
-			data['desc'] = data['title']
-			print(data['desc'])
-		else:
-			pass
-			# print(file)
-			# texts = soup.select('.text')
-			# desc = descs[0].get_text()
-			# data['desc'] = desc
-			# data['title'] = del_tag(titles[0])
-			# data['qas'] = {'question':desc,'answer':del_tag(texts[0])}
-			# print(data)
+			# if len(dls) > 0:
+			# 	for dl in dls:
+			# 		for s in dl.next_siblings:
+			# 			if not s.isspace:
+			# 				soup2 = BeautifulSoup(str(dl),'lxml')
+			# 				dts = soup2.dt
+			# 				dds = soup2.select('dd')
+			# 				dds = del_tag(dds)
+			# 				qas.append({'question':del_tag(dts),'answer':del_tag(s)}) 
+		# data['qas'] = qas						
+		data['title'] = del_tag(soup.select('.poster-caption'))[0]
+			# if len(data['desc']) == 0:
+		data['desc'] = [data['title']]
+			# print(data['qas'])
+			# txt.writelines("-".join(str(data['desc'])))
+			# txt.writelines('\n')
+	print(data['desc'])
 
