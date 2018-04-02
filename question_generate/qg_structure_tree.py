44371e43d75847513daeffbb4ab8a7b3e1d480f9
TonyZhang1002 <15205944@ucdconnect.ie>
2018-04-02 15:35:51 +0800

    规则更新及前100条范例问题生成

--- question_generate/qg_structure_tree.py
+++ question_generate/qg_structure_tree.py
@@ -9,6 +9,8 @@ from pyltp import SementicRoleLabeller
 from pyltp import NamedEntityRecognizer
 from pyltp import Parser
 import codecs
+from tqdm import tqdm
+import time
 
 def readFile():
     file_object = open('/Users/zhangqinyuan/Documents/Mac_Projects/Intelligent-QA-System/descs.txt','rU', encoding="utf-8")
@@ -81,7 +83,7 @@ def generator(List):
         len(useful_labels) == 1 or
         useful_labels[-1].find('简介') != -1
     ):
-        return('什么是' + useful_labels[0] + '?')
+        return '什么是' + useful_labels[0] + '?'
 
     #假如最后一个标签的开头是“什么”或介词如"与"，则不需要生成问题
     elif (
@@ -90,29 +92,41 @@ def generator(List):
         last_label[0] == '如何'
     ):
         str = ''
-        return(str.join(last_label) + '？')
+        return str.join(last_label) + '？'
 
     #假如最后一个标签就是概述或者产品概述
     elif (
         useful_labels[-1] == '概述' or
         useful_labels[-1] == '产品概述'
     ):
-        return(useful_labels[0] +'的' + useful_labels[-2] + '？')
+        return useful_labels[0] +'的' + useful_labels[-2] + '？'
 
     #假如最后一个标签在例外情况（动词开头）之中
     elif useful_labels[-1] in adjVlist:
-        return(useful_labels[0] +'的' + useful_labels[-1] + '有哪些？')
+        return useful_labels[0] +'的' + useful_labels[-1] + '有哪些？'
 
     #假如最后一个标签的最后一个词为管理
     elif last_label[-1] == '管理':
         str = ''
-        return('怎么管理' + useful_labels[0] + '的' + str.join(last_label[:-1]))
+        return '怎么管理' + useful_labels[0] + '的' + str.join(last_label[:-1])
+
+    # 假如最后一个标签的最后一个词为接口
+    elif (
+        last_label[-1] == '接口' or
+        last_label[-1] == '指南'
+    ):
+        str = ''
+        return useful_labels[0] + '的' + str.join(last_label) + '有哪些？'
+
+    #假如最后一个标签是"购买指南"
+    elif useful_labels[-1] == '购买指南':
+        return '怎么购买' + useful_labels[0] + '？'
 
     #假如最后一个标签的开头是动词，则用“怎么... ...”生成问句
     elif last_label_postages[0] is 'v':
         #假如最后一个标签只有一个动词，例如“入门”
         if len(last_label_postages) == 1:
-            return('怎么' + last_label[0] + useful_labels[0] + '？')
+            return '怎么' + last_label[0] + useful_labels[0] + '？'
         else:
             #假如只有一个动词加其他词，例如“管理集群”
             if (
@@ -122,7 +136,7 @@ def generator(List):
                 '与' not in last_label
             ):
                 str = ''
-                return('怎么' + last_label.pop(0) + useful_labels[0] + '的' + str.join(last_label) + '？')
+                return '怎么' + last_label.pop(0) + useful_labels[0] + '的' + str.join(last_label) + '？'
             #假如超过一个动词，例如“访问和使用DWS”
             else:
                 last_label_postages.reverse()
@@ -131,27 +145,27 @@ def generator(List):
                 str = ''
                 #假如只有动词
                 if last_label_postages[-1] is 'v':
-                    return('怎么' + str.join(last_label[:index_of_last_v]) + useful_labels[0] + '？')
+                    return '怎么' + str.join(last_label[:index_of_last_v]) + useful_labels[0] + '？'
                 #假如有其他词
                 else:
-                    return('怎么' + str.join(last_label[:index_of_last_v]) + useful_labels[0] + '的' + str.join(last_label[index_of_last_v:]) + '？')
+                    return '怎么' + str.join(last_label[:index_of_last_v]) + useful_labels[0] + '的' + str.join(last_label[index_of_last_v:]) + '？'
 
     #假如最后一个标签为"与... ..."，使用第一个标签和最后一个标签形成问句
     elif last_label[0] == '与':
-        return(useful_labels[0] + useful_labels[-1] + '？')
+        return useful_labels[0] + useful_labels[-1] + '？'
 
     #假如最后一个标签的开头是其他词汇，则用“... ...有哪些”生成问句
     else:
         str = ''
-        return(useful_labels[0] + '的' + str.join(last_label) + '有哪些？')
+        return useful_labels[0] + '的' + str.join(last_label) + '有哪些？'
 
 print('******************整体测试：**********************')
 questionDict = readFile()
 k = 0
 fo = codecs.open("/Users/zhangqinyuan/Documents/Mac_Projects/Intelligent-QA-System/question_generate/descs_demo.txt", 'r+', encoding = 'utf-8')
-while k < 1000:
-    fo.write(generator(questionDict[k])+'\n')
-    k += 1
+for i in tqdm(range(100)):
+    fo.write(generator(questionDict[i])+'\n')
+    time.sleep(0.01)
 fo.close()
 
 #print('******************分部份测试，将会顺序执行：**********************')
@@ -172,6 +186,5 @@ fo.close()
 #“弹性云服务器 P1型云服务器安装NVIDIA GPU驱动和CUDA工具包 ”：复杂的句式（谓语滞后）
 #“镜像服务有哪些？”：第二个label和最后一个label是同一个label
 #"命名空间管理"："空间管理"被当作一个词处理
-#"购买指南"：
 
 #词性表：
