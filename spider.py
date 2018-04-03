d3c33142cd44ee01c10d62bf71c7550bc2d3cc3f
KeyListener <15205902@ucdconnect.ie>
2018-04-03 23:25:30 +0800

    完工

--- spider.py
+++ spider.py
@@ -7,7 +7,6 @@ import os
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 
-
 def del_tag(strings):
 	dr = re.compile(r'<[^>]+>',re.S)
 	if type(strings) == type([]): 
@@ -22,6 +21,123 @@ def del_tag(strings):
 		s = dr.sub('',strings)
 		return s
 
+def filter(List):
+    useless_labels = ['简介','帮助中心','概览','产品简介','快速入门','FAQ','用户指南']
+    nList = list()
+    for label in List:
+        if label not in useless_labels:
+            nList.append(label)
+#    for label in nList:
+#        print(label + ' ', end = '')
+#    print()
+    return nList;
+
+def segmentor(_segmentor,sentence=''):
+    words = _segmentor.segment(sentence)  # 分词
+    # 可以转换成List 输出
+    words_list = list(words)
+    # _segmentor.release()  # 释放模型
+    return words_list
+
+#词性标注  
+def posttagger(_posttagger,words):  
+    
+    posttags=_posttagger.postag(words)  #词性标注  
+    postags = list(posttags)  
+    # _posttagger.release() #释放模型  
+    #print type(postags)  
+    return postags
+
+#生成问句
+def generator(_segmentor,_postagger,List):
+    labels = List
+    useful_labels = filter(labels)
+    #找到最后一个标签
+    last_label = segmentor(_segmentor,useful_labels[-1])
+    #记录最后一个标签的所有词性
+    last_label_postages = posttagger(_postagger,last_label)
+    #动词做定语
+    adjVlist = ["使用限制","计费方式","操作指南","准备工作","使用限制"]
+
+    #假如只有一个有用标签或最后一个标签包含简介
+    if (
+        len(useful_labels) == 1 or
+        useful_labels[-1].find('简介') != -1
+    ):
+        return '什么是' + useful_labels[0] + '?'
+
+    #假如最后一个标签的开头是“什么”或介词如"与"，则不需要生成问题
+    elif (
+        last_label[0] == '什么' or
+        last_label[0] == '怎么' or
+        last_label[0] == '如何'
+    ):
+        str = ''
+        return str.join(last_label) + '？'
+
+    #假如最后一个标签就是概述或者产品概述
+    elif (
+        useful_labels[-1] == '概述' or
+        useful_labels[-1] == '产品概述'
+    ):
+        return useful_labels[0] +'的' + useful_labels[-2] + '？'
+
+    #假如最后一个标签在例外情况（动词开头）之中
+    elif useful_labels[-1] in adjVlist:
+        return useful_labels[0] +'的' + useful_labels[-1] + '有哪些？'
+
+    #假如最后一个标签的最后一个词为管理
+    elif last_label[-1] == '管理':
+        str = ''
+        return '怎么管理' + useful_labels[0] + '的' + str.join(last_label[:-1])
+
+    # 假如最后一个标签的最后一个词为接口
+    elif (
+        last_label[-1] == '接口' or
+        last_label[-1] == '指南'
+    ):
+        str = ''
+        return useful_labels[0] + '的' + str.join(last_label) + '有哪些？'
+
+    #假如最后一个标签是"购买指南"
+    elif useful_labels[-1] == '购买指南':
+        return '怎么购买' + useful_labels[0] + '？'
+
+    #假如最后一个标签的开头是动词，则用“怎么... ...”生成问句
+    elif last_label_postages[0] is 'v':
+        #假如最后一个标签只有一个动词，例如“入门”
+        if len(last_label_postages) == 1:
+            return '怎么' + last_label[0] + useful_labels[0] + '？'
+        else:
+            #假如只有一个动词加其他词，例如“管理集群”
+            if (
+                '和' not in last_label and
+                '或' not in last_label and
+                '并' not in last_label and
+                '与' not in last_label
+            ):
+                str = ''
+                return '怎么' + last_label.pop(0) + useful_labels[0] + '的' + str.join(last_label) + '？'
+            #假如超过一个动词，例如“访问和使用DWS”
+            else:
+                last_label_postages.reverse()
+                index_of_last_v = len(last_label_postages) - last_label_postages.index('v')
+                last_label_postages.reverse()
+                str = ''
+                #假如只有动词
+                if last_label_postages[-1] is 'v':
+                    return '怎么' + str.join(last_label[:index_of_last_v]) + useful_labels[0] + '？'
+                #假如有其他词
+                else:
+                    return '怎么' + str.join(last_label[:index_of_last_v]) + useful_labels[0] + '的' + str.join(last_label[index_of_last_v:]) + '？'
+#假如最后一个标签为"与... ..."，使用第一个标签和最后一个标签形成问句
+    elif last_label[0] == '与':
+        return useful_labels[0] + useful_labels[-1] + '？'
+
+    #假如最后一个标签的开头是其他词汇，则用“... ...有哪些”生成问句
+    else:
+        str = ''
+        return useful_labels[0] + '的' + str.join(last_label) + '有哪些？'
 
 path = '/home/wangfeihong/桌面/support.huaweicloud.com/'
 
@@ -95,18 +211,10 @@ for file in tqdm(files):
 			# print(questions[i+1][0])
 			content = details.split(str(questions[i][0]))[1]
 			content = content.split(str(questions[i+1][0]))[0] 
-			qas[del_tag(questions[i][0])] = del_tag(content)
+			qas[del_tag(questions[i][0])] = content
 		data['url'] = file
 		data['qas'] = qas
-		print(data)
-
-			# for key,value in qas.items():
-			# 	print(key)
-			# 	print('========================================')
-			# 	print(value)
-			# txt.writelines("".join(str(data['desc'])))
-			# txt.writelines('\n')
-
+	
 	# developer
 	else:
 		soup = BeautifulSoup(text,'lxml')
@@ -124,7 +232,7 @@ for file in tqdm(files):
 					desc.append(del_tag(c))
 			data['desc'] = desc
 		else:
-			data['desc'] = del_tag(str(soup.h1))
+			data['desc'] = [del_tag(str(soup.h1))]
 		# if h1 is None:
 		h5s = soup.select('h5')
 		h4s = soup.select('h4')
@@ -147,11 +255,8 @@ for file in tqdm(files):
 			# print(questions[i+1][0])
 			content = details.split(str(questions[i][0]))[1]
 			content = content.split(str(questions[i+1][0]))[0] 
-			qas[del_tag(questions[i][0])] = del_tag(content)
-		data['url'] = file	
-		data['qas'] = qas	
-	# txt.writelines('\n')
-	# print(file)
-	print(data)
-	
+			qas[del_tag(questions[i][0])] = content
+		data['url'] = file
+		data['qas'] = qas
+		
 
