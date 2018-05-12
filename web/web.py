3f2c91a1219a6b56a80597007e0f208e5029b469
TonyZhang1002 <15205944@ucdconnect.ie>
2018-05-12 17:51:36 +0800

    make messages scalable

--- web/web.py
+++ web/web.py
@@ -173,7 +173,7 @@ def chat():
 
 @app.route('/getanswer', methods=['GET', 'POST'])
 def getanswer():
-	db = pymysql.connect("localhost","root","970429","test",charset="utf8mb4")
+	db = pymysql.connect("localhost","root","jk123456","test",charset="utf8mb4")
 	cursor = db.cursor()
 	question = request.args['question']
 	query = question
@@ -231,14 +231,14 @@ def getanswer():
 		cursor.execute(sql)
 		result = cursor.fetchall()
 		title = result[0][0]
-		answer = '<h2>' + title + '</h2>' + result[0][1]
+		answer = '<h2>' + title + '</h2><d>' + result[0][1]
 		# 当答案中只有一个<a>时,需要加上'链接'让其显示
 		if del_tag(answer) == title:
-			answer = '<h2>' + title + '</h2>' + find_a(answer) + '链接'
+			answer = '<h2>' + title + '</h2><d>' + find_a(answer) + '链接'
 		else:
 			answer = del_div(answer)	
 		answer = answer.replace('</div>','')
-		answer = '<div>' + answer + '<key>' +  str(keywords) + '</key>' + '</div>'
+		answer =  answer + '<key style="color:#fff">' +  str(keywords) + '</key>' 
 		answer = answer.replace(']','') + '<split>'
 
 		# print(answer)
@@ -322,11 +322,11 @@ def getanswer():
 		cursor.execute(sql)
 		result = cursor.fetchall()
 		title = result[0][0]
-		answer = '<h2>' + title + '</h2>' + result[0][1]
+		answer = '<h2>' + title + '</h2><d>' + result[0][1]
 		# print(cursor.fetchone())
 		if del_tag(answer) == title:
-			answer = '<h2>' + title + '</h2><div class="text" style="display:none">' + find_a(answer) + '链接'
-		totalanswer = totalanswer + answer + '<key>' +  str(keywords) + '</key>' + '<split>'
+			answer = '<h2>' + title + '</h2><d>' + find_a(answer) + '链接'
+		totalanswer = totalanswer + answer + '<key style="color:#fff">' +  str(keywords) + '</key>' + '<split>'
 
 	totalanswer = del_div(totalanswer)
 	totalanswer = totalanswer.replace('</div>','')
@@ -342,6 +342,6 @@ if __name__ == '__main__':
 	questionwords = similar_dict.keys()
 	questionwords = list(questionwords)
 #	questionwords.remove('')
-	app.run(host='0.0.0.0',port=5000,debug=True)
+	app.run(host='0.0.0.0',port=8800)
 
 
