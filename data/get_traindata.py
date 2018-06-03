69b2e27aa19971e8e118060507e29579d49cf30c
Adam0429 <872490934@qq.com>
2018-06-03 17:04:32 +0800

    training data2

--- data/get_traindata.py
+++ data/get_traindata.py
@@ -1,13 +1,18 @@
 import pymysql
+import random
 
-dictfile = open('traindata.txt','r+')
+dictfile = open('traindata2.txt','r+')
 db = pymysql.connect("localhost","root","970429","test",charset="utf8mb4")
 cursor = db.cursor()
-cursor.execute('select normal_question from QA where normal_question like "%什么%" or normal_question like "%?%"')
+cursor.execute('select normal_question from QA ')
+# where normal_question' like "%什么%" or normal_question like "%?%"')
 result = cursor.fetchall()
+result = list(result)
+random.shuffle(result)
+print(result)
 text = ''
 for r in result:
-	idx = len(r[0].split('-'))
-	text = text + (r[0].split('-'))[idx-1] + '\n'
-
+	# idx = len(r[0].split('-'))
+	# text = text + (r[0].split('-'))[idx-1] + '\n'
+	text = text + r[0] + '\n'
 dictfile.write(text)
